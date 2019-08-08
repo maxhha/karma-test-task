9b73b99badc318b4b384bde72ccccce43ff23cb7
maxhha <devmaxhha@gmail.com>
2019-08-08 21:45:43 +0300

    Add error block in form

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
 import LoginQuery from "/queries/login.js";
-
+import cn from "classnames";
 
 import "../commons/form.scss";
 import "../commons/icon.scss";
@@ -10,22 +10,34 @@ import "./LoginForm.scss";
 
 class LoginForm extends React.Component {
 
+    state = {
+        error: null,
+    }
+
     usernameRef = React.createRef()
     passwordRef = React.createRef()
 
     onSubmit = (e) => {
         e.preventDefault();
+        this.setState({error: null});
         LoginQuery({
             "username": this.usernameRef.current.value,
             "password": this.passwordRef.current.value
         }).then(() => this.props.history.push('/user'))
-        .catch((e) => console.error(e));
+        .catch((e) => this.setState({error: e.message || e[0].message}));
     }
 
     render () {
         return (
             <form
-                className={`form login-form login-form_long-shadow ${this.props.className}`}
+                className={cn(
+                    "form login-form",
+                    {
+                        "login-form_error login-form_long-shadow-error": !!this.state.error,
+                        "login-form_long-shadow": !this.state.error
+                    },
+                    this.props.className
+                )}
                 onSubmit={this.onSubmit}
             >
                 <div className="form__icon form__icon_long-shadow">
@@ -49,6 +61,9 @@ class LoginForm extends React.Component {
                     required
                 />
                 <input className="form__submit" type="submit" value="LOGIN"/>
+                {this.state.error && (
+                    <div className="form__error">{this.state.error}</div>
+                )}
                 <Link
                     className="form__link"
                     to="/register"
--- src/components/RegisterForm/index.jsx
+++ src/components/RegisterForm/index.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
 import RegisterQuery from "/queries/register.js";
+import cn from "classnames";
 
 import "../commons/form.scss";
 import "../commons/icon.scss";
@@ -9,24 +10,36 @@ import "./RegisterForm.scss";
 
 class RegisterForm extends React.Component {
 
+    state = {
+        error: null,
+    }
+
     usernameRef = React.createRef()
     passwordRef = React.createRef()
     confirmRef = React.createRef()
 
     onSubmit = (e) => {
         e.preventDefault();
+        this.setState({state: null});
         RegisterQuery({
             "username": this.usernameRef.current.value,
             "password": this.passwordRef.current.value,
             "confirm": this.confirmRef.current.value
         }).then(() => this.props.history.push('/user'))
-        .catch((e) => console.error(e));
+        .catch((e) => this.setState({error: e.message || e[0].message}));
     }
 
     render () {
         return (
             <form
-                className={`form register-form register-form_long-shadow ${this.props.className}`}
+                className={cn(
+                    "form register-form",
+                    {
+                        "register-form_error register-form_long-shadow-error": !!this.state.error,
+                        "register-form_long-shadow": !this.state.error
+                    },
+                    this.props.className
+                )}
                 onSubmit={this.onSubmit}
             >
                 <div className="form__icon form__icon_long-shadow">
@@ -62,6 +75,9 @@ class RegisterForm extends React.Component {
                     type="submit"
                     value="REGISTER"
                 />
+                {this.state.error && (
+                    <div className="form__error">{this.state.error}</div>
+                )}
                 <Link
                     className="form__link"
                     to="/login"
